c0b56ae7c96e836d41dd4c87906fff0d2ad865bc
Takayoshi Tanaka <tanaka-takayoshi@users.noreply.github.com>
2020-02-01 16:38:48 +0900

    追加忘れ。

--- webapp/cs/Startup.cs
+++ webapp/cs/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -17,7 +18,26 @@ namespace cs
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            var host = Environment.GetEnvironmentVariable("MYSQL_HOSTNAME");
+            host ??= "127.0.0.1";
+            var port = Environment.GetEnvironmentVariable("MYSQL_PORT");
+            if (!int.TryParse(port, out _))
+                port = null;
+            port ??= "3306";
+            var user = Environment.GetEnvironmentVariable("MYSQL_USER");
+            user ??= "isutrain";
+            var dbname = Environment.GetEnvironmentVariable("MYSQL_DATABASE");
+            dbname ??= "isutrain";
+            var password = Environment.GetEnvironmentVariable("MYSQL_PASSWORD");
+            password ??= "isutrain";
+            var connectionString = $"server={host}:{port};database={dbname};uid={user};pwd={password};charset=utf8mb4";
+            Configuration = new ConfigurationBuilder()
+                .AddConfiguration(configuration)
+                .AddInMemoryCollection(new[]
+                {
+                    KeyValuePair.Create("ConnectionStrings:Isucon9", connectionString)
+                })
+                .Build();
         }
 
         public IConfiguration Configuration { get; }
@@ -26,11 +46,18 @@ namespace cs
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.Configure<RouteOptions>(options => {
+                // URL‚ð¬•¶Žš‚É‚·‚é
+                options.LowercaseUrls = true;
+                options.LowercaseQueryStrings = true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            Console.WriteLine(Configuration.GetConnectionString("Isucon9"));
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -42,9 +69,9 @@ namespace cs
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
+            app.UseEndpoints(endpoints => 
             {
-                endpoints.MapControllers();
+                endpoints.MapControllerRoute("default", "api/{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
--- webapp/cs/WeatherForecast.cs
+++ webapp/cs/WeatherForecast.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 
 namespace cs
